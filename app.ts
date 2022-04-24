cc68d248d687a74da29d85b65cc350a0b885d86d
Shaul Xu <302763630@qq.com>
2022-04-24 18:37:36 +0800

    feat: save file

---
+++ app.ts
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import path from 'path'
+import fs, { ReadStream } from 'fs'
+
+const requestUrl =
+  'https://bing.biturl.top/?resolution=3840&format=json&index=0&mkt=zh-CN'
+
+type Response = {
+  start_date: string
+  end_date: string
+  url: string
+  copyright: string
+  copyright_link: string
+}
+
+const fetchData = async () => {
+  const { data } = await axios.get<Response>(requestUrl)
+  return data
+}
+
+const saveFile = async () => {
+  const image = await fetchData()
+  const archiveDirPath = path.resolve(__dirname, 'archives')
+
+  if (!fs.existsSync(archiveDirPath)) {
+    fs.mkdirSync(archiveDirPath)
+  }
+
+  const subDirPath = path.resolve(archiveDirPath, image.start_date)
+  if (!fs.existsSync(subDirPath)) {
+    fs.mkdirSync(subDirPath)
+  }
+
+  const imageFilePath = path.resolve(
+    subDirPath,
+    `${image.copyright.replace(/\//g, '\u2215')}.jpg`
+  )
+  const metaFilePath = path.resolve(subDirPath, 'meta.json')
+  const { data: stream } = await axios.request<ReadStream>({
+    url: image.url,
+    responseType: 'stream',
+  })
+  const imgWs = fs.createWriteStream(imageFilePath)
+  const jsonWs = fs.createWriteStream(metaFilePath)
+  stream.pipe(imgWs)
+  jsonWs.write(JSON.stringify(image, null, 4))
+}
+
+;(async () => {
+  await saveFile()
+})()
